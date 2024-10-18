6227b92584d70e675dbaa1edd22364ef3704468f
SrinjanaNag12 <srinjananag68@gmail.com>
2024-10-17 23:12:38 -0400

    Code updated

--- variables.tf
+++ variables.tf
@@ -1,23 +1,53 @@
+# Define the AWS region
 variable "aws_region" {
-  description = "Region where resources will be created"
-  default     = "us-east-1"
+  description = "AWS region where resources will be created"
+  default     = "us-east-2"
 }
 
+# Define the VPC CIDR block
 variable "vpc_cidr_block" {
   description = "Base CIDR block for the VPC"
   default     = "10.0.0.0/16"
 }
 
+# Define the availability zones
 variable "availability_zones" {
-  description = "List of availability zones for public and private subnets"
+  description = "List of availability zones"
   type        = list(string)
-  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  default     = ["us-east-2a", "us-east-2b", "us-east-2c"]
 }
 
+# Define the AMI ID for EC2 instance
+variable "ami_id" {
+  description = "AMI ID for EC2 instance"
+  default     = "ami-0b59e1727222f2247"  
+}
+
+# Define the instance type for EC2 instance
+variable "instance_type" {
+  description = "Instance type for EC2"
+  default     = "t2.micro"
+}
+
+# Define the application port
+variable "application_port" {
+  description = "Port where the web application runs"
+  default     = 5001
+}
+
+# Define the root volume size
+variable "root_volume_size" {
+  description = "Root EBS volume size in GB"
+  default     = 25
+}
+
+# Define the VPC name
 variable "name" {
-  type        = string
   description = "Name of the VPC"
-  default     = "Vpc"
+  default     = "vpc2"
 }
 
-
+variable "keyname" {
+  description = "Name of the key"
+  default     = "csye6225"
+}
